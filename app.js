ca39440531401184f6d2377848f4a30a0f18e964
DEFIdaniel <greatprotectionwallet@gmail.com>
2022-07-20 14:17:33 -0600

    project main features complete

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const ejs = require("ejs");
+const app = express();
 const _ = require("lodash");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -8,15 +11,16 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [{}];
-
-const app = express();
-
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
+let posts = [
+  {
+    title: "Blog Post 1",
+    post: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos fuga praesentium accusantium doloremque sed odit laboriosam a non accusamus natus, perspiciatis labore pariatur consectetur obcaecati doloribus. Aliquam inventore quo et recusandae dolore soluta ipsam, repellendus vel ducimus. Enim corporis aliquid aut at quos deserunt debitis voluptate inventore tempora dolorum?",
+  },
+  {
+    title: "Blog Post 2",
+    post: "Officia nobis iure dolorum voluptate, nisi veritatis nulla doloremque esse obcaecati autem ratione voluptatibus laboriosam expedita voluptas, aut deleniti quidem eaque illo! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia nobis iure dolorum voluptate, nisi veritatis nulla doloremque esse obcaecati autem ratione voluptatibus laboriosam expedita voluptas, aut deleniti quidem eaque illo!",
+  },
+];
 app.get("/", (req, res) => {
   res.render("home", { homeContent: homeStartingContent, blogPosts: posts });
 });
@@ -35,10 +39,10 @@ app.get("/post/:postName", (req, res) => {
   for (let i = 0; i < posts.length; i++) {
     let loopTitle = _.lowerCase(posts[i].title);
     if (loopTitle === postTitle) {
-      console.log(posts[i].title);
-      console.log(posts[i].post);
       res.render("post", { heading: posts[i].title, content: posts[i].post });
-    } 
+    } else {
+      res.redirect("/");
+    }
   }
 });
 
